dffe5a8f203fed09e3ad009c261f7c71fba51112
Huy Tran <tanhuy998@gmail.com>
2020-03-29 13:56:55 +0700

    modify(init.php)

--- src/init.php
+++ src/init.php
@@ -1,16 +1,23 @@
 <?php
     putenv('BASE_PATH='.dirname(__DIR__));
 
-    /**
-     *  Base Path is the directory that the project is placed
-     */
-    function BasePath() {
-        return getenv('BASE_PATH');
+    if (!function_exists('BasePath')) {
+        /**
+        *  Base Path is the directory that the project is placed
+        */
+        function BasePath() {
+            return getenv('BASE_PATH');
+        }
     }
+    
+    
 
-    function UseSession() {
-        session_start();
+    if (!function_exists('UseSession')) {
+        function UseSession() {
+            session_start();
+        }
     }
+    
 
     if (file_exists(BasePath().'/.env')) {
         $env = file(getenv('BASE_PATH').'/.env');
@@ -22,29 +29,35 @@
         unset($env);
     }
 
-    /**
-     *  Sub-root directory is the sub directory
-     *  of $_SERVER['DOCUMENT_ROOT'] directory
-     *  sub-root also known as the base path without the document root directory
-     */
-    function SubRootDir() {
-        $root_dir = $_SERVER['DOCUMENT_ROOT'];
-        $base_path = str_replace('\\','/',BasePath());
+    if (!function_exists('SubRootDir')) {
+        /**
+        *  Sub-root directory is the sub directory
+        *  of $_SERVER['DOCUMENT_ROOT'] directory
+        *  sub-root also known as the base path without the document root directory
+        */
+        function SubRootDir() {
+            $root_dir = $_SERVER['DOCUMENT_ROOT'];
+            $base_path = str_replace('\\','/',BasePath());
         
-        $ret = str_replace($root_dir,'', $base_path);
+            $ret = str_replace($root_dir,'', $base_path);
 
-        $first_char = substr($ret,0,1);
-        $last_char = substr($ret, strlen($ret)-1, 1);
+            $first_char = substr($ret,0,1);
+            $last_char = substr($ret, strlen($ret)-1, 1);
 
-        $ret = $first_char === '/'? substr($ret, 1, strlen($ret)-1): $ret;
-        $ret = $last_char === '/'? substr($ret, strlen($ret)-1, 1): $ret;
+            $ret = $first_char === '/'? substr($ret, 1, strlen($ret)-1): $ret;
+            $ret = $last_char === '/'? substr($ret, strlen($ret)-1, 1): $ret;
 
-        return $ret;
+            return $ret;
+        }
     }
+    
 
-    function CurrentLocation() {
-        return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    if (!function_exists('CurrentLocation')) {
+        function CurrentLocation() {
+            return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        }
     }
+    
 
     
 
