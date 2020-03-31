470445d35ed4af679ed11da9553e791b5d533a8c
Huy Tran <tanhuy998@gmail.com>
2020-03-31 23:30:19 +0700

    modify(Event, EventEmitter)

--- src/dependencies/event/event.php
+++ src/dependencies/event/event.php
@@ -1,2 +1,23 @@
 <?php 
-    
+    namespace Dependencies\Event;
+
+use Closure;
+
+    class Event {        
+        
+        private $listener;
+        protected $emitter;
+
+        public function __construct(EventEmitter $_emitter) {
+            
+            $this->emitter = $_emitter;
+        }
+
+        public function Do(Closure $_func) {
+            $this->listener = $_func;
+        }
+
+        public function GetListener() {
+            return $this->listener;
+        }
+    }
--- src/dependencies/event/eventemitter.php
+++ src/dependencies/event/eventemitter.php
@@ -1,25 +1,46 @@
 <?php
-    namespace Dependencies\event;
+    namespace Dependencies\Event;
 
+    use Dependencies\Event\Event as Event;
     use Closure;
+    use Exception;
 
     abstract class EventEmitter {
 
         private $events;
 
-        public function EventExist(string $_name): bool {
+        public final function EventExist(string $_name): bool {
             return array_key_exists($_name, $this->events);
         }
 
-        protected function SetEvent(string $_event_name) {
+        protected final function AddEvent(string $_event_name) {
 
+            if (!$this->EventExist($_event_name)) {
+                $this->events[$_event_name] = new Event($this);
+            }
         }
 
-        protected function AddEventlistener(string $_event_name, Closure $_callback) {
+        protected final function AddEventlistener(string $_event_name, Closure $_callback) {
 
+            if (!array_key_exists($_event_name, $this->events)) {
+
+                $this->AddEvent($_event_name);
+            }
+            $this->On($_event_name)->Do($_callback);
         }
 
-        public function On(string $_event_name) {
-            if (array_key_exists($_event_name, $this->events));
+        protected final function Emit($_event_name) {
+            $listeners = $this->On($_event_name)->Getlistener();
+
+            foreach ($listeners as $listener) {
+                $listener();
+            }
+        }
+
+        public final function On(string $_event_name): Event {
+            if (array_key_exists($_event_name, $this->events)) return $this->events[$_event_name];
+
+            $class = get_class($this);
+            throw new Exception("Call on undefined Event \'$_event_name\' of class \'$class\'");
         }
     }
